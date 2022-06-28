c1eff74afa2a88e4f3539694d67e9ab549c9f905
EmanuelDicu <Emanuel.Dicu@softwire.com>
2022-06-28 16:30:48 +0100

    Work in progress: implements complex fizz-buzz game using visitor design pattern

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,17 +1,79 @@
-function isFizz(x: number): boolean {
-    return x % 3 === 0;
+interface FizzBuzzVisitorInt {
+    function visit(x: FizzBuzzNumber);
 }
 
-function isBuzz(x: number): boolean {
-    return x % 5 === 0;
+interface IntFizzBuzzNumber {
+    function accept(visitor: FizzBuzzVisitorInt);
+}
+
+class FizzBuzzNumber implements IntFizzBuzzNumber {
+    value: number;
+    output: string;
+
+    constructor(value: number) {
+        this.value = value;
+        this.output = "";
+    }
+
+    function accept(visitor: FizzBuzzVisitorInt) {
+        visitor.visit(this);
+    }
+}
+
+class FizzVisitor implements FizzBuzzVisitorInt {
+    function visit(num: FizzBuzzNumber) {
+        if (num.value % 3 == 0)
+            num.output += "Fizz";
+    }
+}
+
+class BuzzVisitor implements FizzBuzzVisitorInt {
+    function visit(num: FizzBuzzNumber) {
+        if (num.value % 5 == 0)
+            num.output += "Buzz";
+    }
+}
+
+class BangVisitor implements FizzBuzzVisitorInt {
+    function visit(num: FizzBuzzNumber) {
+        if (num.value % 7 == 0)
+            num.output += "Buzz";
+    }
+}
+
+class BongVisitor implements FizzBuzzVisitorInt {
+    function visit(num: FizzBuzzNumber) {
+        if (num.value % 11 == 0)
+            num.output = "Bong";
+    }
+}
+
+class FezzVisitor implements FizzBuzzVisitorInt {
+    function visit(num: FizzBuzzNumber) {
+        if (num.value % 13 != 0)
+            return;
+        let position: number = num.output.length;
+        for (let i = 0; i < num.output.length; ++i) {
+            if (num.output.charAt(i).toLowerCase() === "b") {
+                position = i;
+                break;
+            }
+        }
+        num.output = num.output.slice(0, position) + "Fezz" + num.output.slice(position);
+    }
+}
+
+class ReverseVisitor implements FizzBuzzVisitorInt {
+    function visit(num: FizzBuzzNumber) {
+
+    }
 }
 
 function numberToFizzBuzz(x: number): string {
     let output: string = "";
-    if (isFizz(x))
-        output += "Fizz";
-    if (isBuzz(x))
-        output += "Buzz";
+    output = applyFizz(x, output);
+    output = applyBuzz(x, output);
+    output = applyBang(x, output);
     if (!output.length)
         output = x.toString();
     return output;
